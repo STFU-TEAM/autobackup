7ae54f21b5167e6d8ab1093ab161017e400c9119
EIRBLAST <64648494+EIRBLAST@users.noreply.github.com>
2021-11-05 00:31:09 +0100

    :egg: initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python backup.py
---
+++ backup.py
@@ -0,0 +1,41 @@
+import asyncpg
+import gzip
+import datetime
+import asyncio
+import os
+
+URL = os.environ["URL"]
+PATH = os.environ["PATH"]
+TABLES = ["global", "ban", "gang", "servers", "users", "webhook"]
+
+
+async def backup_loop():
+    try:
+        today = datetime.datetime.date(datetime.datetime.min)
+        while True:
+            # check if the backup has been run today
+            if today != datetime.datetime.date(datetime.datetime.now()):
+                today = datetime.datetime.date(datetime.datetime.now())
+                # connect to the database
+                connection: asyncpg.Connection = await asyncpg.connect(
+                    URL, ssl="require"
+                )
+                # database
+                for table in TABLES:
+                    result = await connection.copy_from_table(
+                        table, output=f"{str(today)}-{table}.csv", format="csv"
+                    )
+                    # compress backup gzip file
+                    with gzip.open(f"{PATH}{str(today)}-{table}.csv.gz", "wb") as f:
+                        f.write(result)
+                # close connection
+                await connection.close()
+                print(f"{str(today)} backup: done")
+    except Exception as exc:
+        print(exc)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(backup_loop(), loop=loop)
+    loop.run_forever()
